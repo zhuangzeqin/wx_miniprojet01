adb17b1878911c1a92b2ded274f2775d7bbbf6a5
zhuangzeqin <zzq@eeepay.cn>
2021-01-29 09:01:20 +0800

    js判断json数据是否存在某字段的方法

--- pages/demo18/demo18.js
+++ pages/demo18/demo18.js
@@ -5,7 +5,7 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        res:{"header" :{"errMsg":'更新三码(开户名、银行卡号、身份证号)审核次数已达上限可进入下一步!',"error":"温馨提示" , "succeed" :true}}
     },
 
     /**
@@ -14,6 +14,39 @@ Page({
     onLoad: function (options) {
         console.log("onLoad");
         //onLoad 中发送异步请求 初始化数据
+        // let res = res?.body?.authAcqCode;//鉴权通道标识
+        // console.log("onLoad:"+res);
+        let  res ={"header" :{"errMsg":'更新三码(开户名、银行卡号、身份证号)审核次数已达上限可进入下一步!',"error":"温馨提示" , "succeed" :true}};
+       let str =  res.hasOwnProperty("body")?'aaa':"bbb";
+       console.log(str);
+
+
+        // if (this.data.res.hasOwnProperty("body")) {
+        //     console.log("onLoad:"+res.body);
+        //     //存在
+        // } else {
+        //     //不存在
+        //     console.log("onLoad:不存在"+res);
+        // }
+
+        // var json = {"key1":"val1","key2":"val2","key3":"val3"};
+        
+
+// if(json.hasOwnProperty("key1")){  
+
+// console.log(json["key1]);
+
+// }
+
+// js判断json数据是否存在某字段的方法
+// 方式一 !("key" in obj) 
+// 复制代码
+// if("name" in json){//json就是数组，name是你要找的值
+//     console.log("有")
+// }else{
+//     console.log("无")
+// }
+// 方式二 obj.hasOwnProperty("key")  //obj为json对象。
     },
 
     /**
